88c865a4a4074fb35f185d86efad2357dbb4a151
secretLJW <secretljw@gmail.com>
2016-04-26 23:32:36 +0900

    fucking2

--- mr-answerer-bot.js
+++ mr-answerer-bot.js
@@ -86,6 +86,13 @@
 				});
 			}
 
+			if ($('#forest-banner').html() != undefined)
+			{
+				$('.next_btn').click();
+				location.href="http://www.memrise.com/home/";
+			}
+
+
 			return;
 		}
 
